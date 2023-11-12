3b20328ee4b0f62d184a6903b08278d56f196bc2
iNBGi <132969754+iNBGi@users.noreply.github.com>
2023-11-12 22:18:09 +0800

    bago nagana

---
+++ react-main/app/screen/Register/login.style.js
@@ -0,0 +1,26 @@
+import{ StyleSheet } from "react-native";
+
+export const loginStyle = StyleSheet.create({
+    content: {
+        display: "flex",
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        backgroundColor:"#ccdad1"
+    },
+    view: {
+        width: "80%"
+    },
+    cardTitle: {
+        color: "#9caea9"
+    },
+    cardButton: {
+        margin: 2,
+        marginLeft: 0,
+        marginRight: 0,
+        backgroundColor:"#ccdad1"
+    },
+    
+    
+})
---
+++ react-main/app/screen/login/login.style.js
@@ -0,0 +1,26 @@
+import{ StyleSheet } from "react-native";
+
+export const loginStyle = StyleSheet.create({
+    content: {
+        display: "flex",
+        flex: 1,
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:"row",
+        backgroundColor:"#33e5b5"
+    },
+    view: {
+        width: "80%"
+    },
+    cardTitle: {
+        color: "#6684a1"
+    },
+    cardButton: {
+        margin: 2,
+        marginLeft: 0,
+        marginRight: 0,
+        backgroundColor:"#6684a1"
+    },
+    
+    
+})
